5c2b56fc76a1e9ddb1dbfa3e690e0516c5045f35
Justus446 <omondijustus056@gmail.com>
2024-07-30 15:46:17 +0300

    new layout

--- sign.py
+++ sign.py
@@ -1,28 +1,65 @@
-# from moviepy.editor import *
-
 from kivy.app import App
-import sys
-from kivy.clock import Clock
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.uix.label import Label
-from kivy.uix.video import Video
 from kivy.lang import Builder
+from kivy.uix.video import Video
+from kivy.clock import Clock
+from kivy.uix.label import Label
 import speech_recognition as sr
+import os
 
 Clock.max_iteration = 20
 
-span_list = ['bueno', 'padre', 'yo', 'intiendo', 'tu', 'no', 'si']
-
+Builder.load_file('smain.kv')
 
 class MainWindow(Screen):
-    # QUIT button
     def quit(self):
-        sys.exit(0)
+        App.get_running_app().stop()
 
+    def speak(self):
+        self.ids.output_label.text = "Listening..."
+        self.listen(self.ids.lang_spinner.text)
+
+    def translate(self):
+        lang = self.ids.lang_spinner.text
+        self.ids.output_label.text = f"Translating from {lang}..."
+        self.ids.video_display.source = 'sample_video.mp4'
+        self.ids.video_display.state = 'play'
+
+    def listen(self, language):
+        recognizer = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something")
+            audio = recognizer.listen(source)
+            recognizer.adjust_for_ambient_noise(source)
+
+        print("Processing")
+        try:
+            recognized_text = recognizer.recognize_google(audio)
+            print(recognized_text)
+            recognized_words = set(recognized_text.lower().split())
+            if language == 'English':
+                known_words = {'bad', 'begin', 'father', 'fine', 'good', 'i', 'like', 'mother', 'no', 'you', 'yes', 'know', 'understand'}
+            else:
+                known_words = {'bueno', 'padre', 'yo', 'intiendo', 'tu', 'no', 'si'}
+
+            if recognized_words.intersection(known_words):
+                self.ids.output_label.text = recognized_text
+                for word in recognized_text.split():
+                    video_file = f'{word}.mp4'
+                    if os.path.exists(video_file):
+                        vid = Video(source=video_file, pos_hint={'center_x': .3, 'top': 1}, size_hint=(.9, .8))
+                        vid.state = 'play'
+                        self.add_widget(vid)
+            else:
+                self.ids.output_label.text = "WORD NOT FOUND"
+        except sr.UnknownValueError:
+            self.ids.output_label.text = "Sorry, I did not understand that."
+        except sr.RequestError:
+            self.ids.output_label.text = "Sorry, there was an error with the request."
 
 class Window1(Screen):
     def quit(self):
-        sys.exit(0)
+        App.get_running_app().stop()
 
     def help(self):
         vid = Video(source='justus.mp4', pos_hint={'center_x': .3, 'top': 1}, size_hint=(.9, .8))
@@ -41,7 +78,6 @@ class Window1(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
         self.r = sr.Recognizer()
-        self.audio = 'dfghjk'
         self.eng_list = ['bad', 'begin', 'father', 'fine', 'good', 'i', 'like', 'mother', 'no', 'you', 'yes', 'know',
                          'understand']
         self.span_list = ['bueno', 'padre', 'yo', 'intiendo', 'tu', 'no', 'si']
@@ -49,103 +85,12 @@ class Window1(Screen):
         self.eng_set = {}
         self.span_set = {}
 
-    # turn on mic and listen
-    def english(self):
-        with sr.Microphone() as source:
-            print("say something")
-            audio = self.r.listen(source)
-
-            self.r.adjust_for_ambient_noise(source)
-
-        print("good")
-
-        lis = self.r.recognize_google(audio)
-
-        print(lis)
-        lis = str(lis)
-
-        print(lis.split(" "))
-        lis = lis.split()
-
-        audio_ = set(lis)
-        print(audio_)
-
-        self.eng_set = set(self.eng_list)
-        print(self.eng_set)
-        if audio_.intersection(self.eng_set):
-            print("done")
-
-            self.text = str(lis)
-            lbl = Label(text=self.text, font_size='20sp', pos_hint={'x': 0, 'y': .1}, size_hint=(.6, .099))
-            self.add_widget(lbl)
-            # audio = list(audio)
-
-            for word in lis:
-                word = word + '.mp4'
-                print(word)
-                vid = Video(source=word, pos_hint={'center_x': .3, 'top': 1}, size_hint=(.9, .8))
-                vid.state = 'play'
-                self.add_widget(vid)
-
-        else:
-            print("not done")
-            lbl = Label(text="WORD NOT FOUND", font_size='20sp', pos_hint={'x': 0, 'y': .1}, size_hint=(.6, .099))
-            self.add_widget(lbl)
-
-    def spanish(self):
-        with sr.Microphone() as source:
-            print("say something")
-            audio = self.r.listen(source)
-
-            self.r.adjust_for_ambient_noise(source)
-
-        print("good")
-
-        lis = self.r.recognize_google(audio)
-
-        print(lis)
-        lis = str(lis)
-
-        print(lis.split(" "))
-        lis = lis.split()
-
-        audio_ = set(lis)
-        print(audio_)
-
-        self.span_set = set(self.span_list)
-        print(self.span_set)
-        if audio_.intersection(self.span_set):
-            print("done")
-
-            self.text = str(lis)
-            lbl = Label(text=self.text, font_size='20sp', pos_hint={'x': 0, 'y': .1}, size_hint=(.6, .099))
-            self.add_widget(lbl)
-            # audio = list(audio)
-
-            for word in lis:
-                word = word + '.mp4'
-                print(word)
-                vid = Video(source=word, pos_hint={'center_x': .3, 'top': 1}, size_hint=(.9, .8))
-                vid.state = 'play'
-                self.add_widget(vid)
-
-        else:
-            print("not done")
-            lbl = Label(text="WORD NOT FOUND", font_size='20sp', pos_hint={'x': 0, 'y': .1}, size_hint=(.6, .099))
-            self.add_widget(lbl)
-
-
 class WindowManager(ScreenManager):
     pass
 
-
-kv = Builder.load_file('smain.kv')
-
-
 class MainApp(App):
     def build(self):
-        return kv
-
+        return WindowManager()
 
 if __name__ == "__main__":
     MainApp().run()
